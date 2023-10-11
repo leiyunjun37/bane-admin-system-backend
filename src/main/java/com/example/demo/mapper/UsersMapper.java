491af8fc965973ec21d0f44669cc71f25eb654b1
leiyunjun37 <L17857308362@163.com>
2023-10-11 10:33:36 +0800

    cs

--- src/main/java/com/example/demo/mapper/UsersMapper.java
+++ src/main/java/com/example/demo/mapper/UsersMapper.java
@@ -3,9 +3,6 @@ package com.example.demo.mapper;
 import com.example.demo.entity.Users;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 
 /**
